47faf5999710bce5f4299fb64546033602fc6af9
Stavros Kiagias <kiagias@protonmail.com>
2020-11-19 14:22:49 +0200

    Added environment vars

--- intro/pages/api/_lib/karavaki.js
+++ intro/pages/api/_lib/karavaki.js
@@ -1,6 +1,7 @@
 const mongo = require('mongodb').MongoClient;
 
 module.exports = async () => {
+    console.log(process.env)
     return new Promise((resolve,reject)=>{
         mongo.connect(process.env.MONGODB_STRING, {useUnifiedTopology: true}, async (err, client)=>{
             if(err) reject("Couldn\'t connect to the database!")
