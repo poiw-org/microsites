e3716735d4b0bb0ee0d98d5beeb481e1f62d24f5
Stavros Kiagias <kiagias@protonmail.com>
2020-11-22 21:32:46 +0200

    Added sentry

--- cas/pages/_app.js
+++ cas/pages/_app.js
@@ -4,6 +4,13 @@ import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
+import * as Sentry from '@sentry/browser';
+import * as Tracing from '@sentry/tracing';
+
+Sentry.init({
+  dsn: "https://7fd410f5921c46b5a9af7d86a6bc9d67@o350531.ingest.sentry.io/5528008",
+  tracesSampleRate: 1.0,
+})
 
 function CAS({ Component, pageProps }) {
   return <Component {...pageProps} />
